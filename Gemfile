3e39c0a04182e295074f8ed5f1686e84235866a1
vkostyrka <vkosty@softserveinc.com>
2022-07-26 21:48:20 +0300

    add devise and create home controller

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "2.7.1"
 
+gem "devise"
+gem 'devise-jwt'
 gem "rails", "7.0.3.1"
 gem "sqlite3", "~> 1.4"
 gem "puma", "~> 5.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope :api, defaults: { format: :json } do
+    resources :home, only: [:index]
+  end
 end
